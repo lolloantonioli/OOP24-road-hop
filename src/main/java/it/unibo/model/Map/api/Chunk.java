fd3623d40106a5546c8c4d422cad96a2869a115b
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-02-27 18:27:14 +0100

    Defined basic methods in Chunk api

--- src/main/java/it/unibo/model/Map/api/Chunk.java
+++ src/main/java/it/unibo/model/Map/api/Chunk.java
@@ -1,5 +1,15 @@
 package it.unibo.model.Map.api;
 
+import it.unibo.model.Map.util.ChunkType;
+
 public interface Chunk {
+
+    ChunkType getType();
+
+    int getPosition();
+
+    int getHeight();
+
+    int getWidth();
 
 }
