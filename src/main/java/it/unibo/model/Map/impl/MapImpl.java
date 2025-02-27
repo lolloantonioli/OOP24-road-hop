2c528c5791c7059186257b322cf0245669fad484
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-02-27 18:48:27 +0100

    Created the implementations of apis

---
+++ src/main/java/it/unibo/model/Map/impl/MapImpl.java
@@ -0,0 +1,34 @@
+package it.unibo.model.Map.impl;
+
+import java.util.List;
+
+import it.unibo.model.Map.api.Chunk;
+import it.unibo.model.Map.api.Map;
+
+public class MapImpl implements Map {
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void generateNewChunk() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateNewChunk'");
+    }
+
+    @Override
+    public List<Chunk> getVisibleChunks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVisibleChunks'");
+    }
+
+    @Override
+    public List<Chunk> getAllChunks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllChunks'");
+    }
+
+}
