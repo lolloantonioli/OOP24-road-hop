062a9c87c1bee4d62cff6fd4056bea37d1719930
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-02-28 17:29:49 +0100

    Created constructor and fields

--- src/main/java/it/unibo/model/Map/impl/MapImpl.java
+++ src/main/java/it/unibo/model/Map/impl/MapImpl.java
@@ -1,11 +1,24 @@
 package it.unibo.model.Map.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.unibo.model.Map.api.Chunk;
 import it.unibo.model.Map.api.Map;
 
 public class MapImpl implements Map {
+
+    private final List<Chunk> chunks;
+    private final int viewportHeight;
+    private final int viewportWidth;
+    private int currentPosition;
+
+    public MapImpl(final int width, final int height) {
+        this.viewportWidth = width;
+        this.viewportHeight = height;
+        this.currentPosition = 0;
+        this.chunks = new ArrayList<>();
+    }
 
     @Override
     public void update() {
