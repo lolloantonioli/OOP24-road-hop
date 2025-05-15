5c7f4512b98d3f20e4d47ead7233475c94971c5b
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-15 17:16:45 +0200

    Impemented ChunkFactoryImpl

---
+++ src/main/java/it/unibo/model/Map/impl/ChunkFactoryImpl.java
@@ -0,0 +1,38 @@
+package it.unibo.model.Map.impl;
+
+import it.unibo.model.Map.api.Chunk;
+import it.unibo.model.Map.api.ChunkFactory;
+
+public class ChunkFactoryImpl implements ChunkFactory {
+
+    @Override
+    public Chunk createRandomChunk(int position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRandomChunk'");
+    }
+
+    @Override
+    public Chunk createGrassChunk(int position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createGrassChunk'");
+    }
+
+    @Override
+    public Chunk createRoadChunk(int position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRoadChunk'");
+    }
+
+    @Override
+    public Chunk createRailwayChunk(int position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRailwayChunk'");
+    }
+
+    @Override
+    public Chunk createRiverChunk(int position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRiverChunk'");
+    }
+
+}
