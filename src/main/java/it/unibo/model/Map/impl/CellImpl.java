f5b6564fb3193c0ca17cb575c430995c86f76e2a
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-15 17:32:03 +0200

    Implemented Cell api

---
+++ src/main/java/it/unibo/model/Map/impl/CellImpl.java
@@ -0,0 +1,50 @@
+package it.unibo.model.Map.impl;
+
+import java.util.Optional;
+
+import it.unibo.model.Map.api.Cell;
+import it.unibo.model.Map.api.GameObject;
+
+public class CellImpl implements Cell{
+
+    public CellImpl(int x, int position) {
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean addObject(GameObject obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addObject'");
+    }
+
+    @Override
+    public void removeObject() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeObject'");
+    }
+
+    @Override
+    public boolean hasObject() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasObject'");
+    }
+
+    @Override
+    public Optional<GameObject> getContent() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getContent'");
+    }
+
+    @Override
+    public int getX() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getX'");
+    }
+
+    @Override
+    public int getY() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getY'");
+    }
+
+}
