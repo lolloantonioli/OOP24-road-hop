b1af7c4e0dd13cbe52972586d91b914c3387f9c9
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-15 17:45:27 +0200

    Partial implementation of GameObject

---
+++ src/main/java/it/unibo/model/Map/impl/GameObjectImpl.java
@@ -0,0 +1,73 @@
+package it.unibo.model.Map.impl;
+
+import it.unibo.model.Map.api.GameObject;
+
+public class GameObjectImpl implements GameObject {
+
+    private int x;
+    private int y;
+    private boolean movable;
+    private int speed;
+    private boolean platform;
+
+    public GameObjectImpl(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+        this.movable = false;
+        this.speed = 0;
+        this.platform = false;
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean collidesWith(int px, int py) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'collidesWith'");
+    }
+
+    @Override
+    public int getX() {
+        return this.x;
+    }
+
+    @Override
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    @Override
+    public boolean isMovable() {
+        return this.movable;
+    }
+
+    @Override
+    public void setMovable(final boolean movable) {
+        this.movable = movable;
+    }
+
+    @Override
+    public void setSpeed(final int speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public boolean isPlatform() {
+        return this.platform;
+    }
+
+    @Override
+    public void setPlatform(final boolean platform) {
+        this.platform = platform;
+    }
+
+}
