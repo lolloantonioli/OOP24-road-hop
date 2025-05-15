283dac9b361d39dffe4d325217a748c297183c4a
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-15 16:22:27 +0200

    Created Cell api

---
+++ src/main/java/it/unibo/model/Map/api/Cell.java
@@ -0,0 +1,19 @@
+package it.unibo.model.Map.api;
+
+import java.util.Optional;
+
+public interface Cell {
+
+    public boolean addObject(GameObject obj);
+
+    public void removeObject();
+
+    public boolean hasObject();
+
+    public Optional<GameObject> getContent();
+
+    public int getX();
+    
+    public int getY();
+
+}
