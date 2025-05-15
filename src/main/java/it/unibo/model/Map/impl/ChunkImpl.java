81d87c2a4707bf6b50994cc054469451f9bdcaba
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-15 17:31:17 +0200

    Implemented methods of ChunkImpl

---
+++ src/main/java/it/unibo/model/Map/impl/ChunkImpl.java
@@ -0,0 +1,63 @@
+package it.unibo.model.Map.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import it.unibo.model.Map.api.Cell;
+import it.unibo.model.Map.api.Chunk;
+import it.unibo.model.Map.api.GameObject;
+import it.unibo.model.Map.util.ChunkType;
+
+public class ChunkImpl implements Chunk {
+
+    private final List<Cell> cells;
+    private final ChunkType type;
+    private final int position;
+
+    public static final int CELLS_PER_ROW = 6;
+
+    public ChunkImpl(final int position, final ChunkType type) {
+        this.position = position;
+        this.type = type;
+        this.cells = IntStream.range(0, CELLS_PER_ROW)
+            .mapToObj(x -> new CellImpl(x, position))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean addObjectAt(final GameObject obj, final int cellX) {
+        return (cellX >= 0 && cellX < CELLS_PER_ROW) && cells.get(cellX).addObject(obj);
+    }
+
+    @Override
+    public List<GameObject> getObjects() {
+        return cells.stream()
+            .filter(Cell::hasObject)
+            .map(Cell::getContent)
+            .flatMap(Optional::stream)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Cell> getCells() {
+        return this.cells;
+    }
+
+    @Override
+    public Cell getCellAt(final int cellX) {
+        return (cellX >= 0 && cellX < CELLS_PER_ROW) ? cells.get(cellX) : null;
+    }
+
+    @Override
+    public ChunkType getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getPosition() {
+        return this.position;
+    }
+
+}
