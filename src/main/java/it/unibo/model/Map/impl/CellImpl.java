5e674dbad16eaaef93078746d5c50c6d071937b9
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-15 18:00:35 +0200

    Implemented CellImpl methods

--- src/main/java/it/unibo/model/Map/impl/CellImpl.java
+++ src/main/java/it/unibo/model/Map/impl/CellImpl.java
@@ -5,46 +5,50 @@ import java.util.Optional;
 import it.unibo.model.Map.api.Cell;
 import it.unibo.model.Map.api.GameObject;
 
-public class CellImpl implements Cell{
+public class CellImpl implements Cell {
 
-    public CellImpl(int x, int position) {
-        //TODO Auto-generated constructor stub
+    private Optional<GameObject> content;
+    private final int x;
+    private final int y;
+
+    public CellImpl(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+        this.content = Optional.empty();
     }
 
     @Override
-    public boolean addObject(GameObject obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addObject'");
+    public boolean addObject(final GameObject obj) {
+        if (!content.isPresent()) {
+            content = Optional.of(obj);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void removeObject() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeObject'");
+        content = Optional.empty();
     }
 
     @Override
     public boolean hasObject() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'hasObject'");
+        return content.isPresent();
     }
 
     @Override
     public Optional<GameObject> getContent() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getContent'");
+        return this.content;
     }
 
     @Override
     public int getX() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getX'");
+        return this.x;
     }
 
     @Override
     public int getY() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getY'");
+        return this.y;
     }
 
 }
