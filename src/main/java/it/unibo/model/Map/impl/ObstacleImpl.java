25bd2c582c71dcf79fd81527dc5ecb369e44c938
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-15 17:55:03 +0200

    Implemented ObstacleImpl

---
+++ src/main/java/it/unibo/model/Map/impl/ObstacleImpl.java
@@ -0,0 +1,21 @@
+package it.unibo.model.Map.impl;
+
+import it.unibo.model.Map.api.Obstacle;
+import it.unibo.model.Map.util.ObstacleType;
+
+public class ObstacleImpl extends GameObjectImpl implements Obstacle {
+
+    private final ObstacleType type;
+
+    public ObstacleImpl(final int x, final int y, final ObstacleType type, final boolean movable) {
+        super(x, y);
+        this.type = type;
+        setMovable(movable);
+    }
+
+    @Override
+    public ObstacleType getType() {
+        return this.type;
+    }
+
+}
