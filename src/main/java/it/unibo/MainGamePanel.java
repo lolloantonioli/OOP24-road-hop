f4a3cf5ccbf985a1eba73b6b23e8d6baf620cd1f
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-05-16 17:11:11 +0200

    added gamestatemanager to MainGamePanel

--- src/main/java/it/unibo/MainGamePanel.java
+++ src/main/java/it/unibo/MainGamePanel.java
@@ -1,5 +1,16 @@
 package it.unibo;
 
+import it.unibo.controller.GameState;
+import it.unibo.controller.GameStateManager;
+
 public class MainGamePanel {
 
+    private final GameStateManager gameStateManager;
+
+    public MainGamePanel() {
+        // Initialize the game state manager
+        this.gameStateManager = new GameStateManager();
+        this.gameStateManager.setState(GameState.MENU);
+    }
+    
 }
