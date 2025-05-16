b59a01c9f5d22e3406db3f0b108bb6c323bc7ba5
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-16 14:25:07 +0200

    Implemented ObjectPlacer implementation

---
+++ src/main/java/it/unibo/model/Map/impl/ObjectPlacerImpl.java
@@ -0,0 +1,22 @@
+package it.unibo.model.Map.impl;
+
+import it.unibo.model.Map.api.Chunk;
+import it.unibo.model.Map.api.Collectible;
+import it.unibo.model.Map.api.ObjectPlacer;
+import it.unibo.model.Map.api.Obstacle;
+
+public class ObjectPlacerImpl implements ObjectPlacer {
+
+    @Override
+    public void placeObstacles(final Chunk chunk) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'placeObstacles'");
+    }
+
+    @Override
+    public void placeCollectibles(final Chunk chunk) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'placeCollectibles'");
+    }
+
+}
