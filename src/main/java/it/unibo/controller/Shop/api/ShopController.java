63ab8027d7fb8c8ae1d4b8164dc8e94587f7847a
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-05-16 16:51:01 +0200

    Added some methods to ShopController

--- src/main/java/it/unibo/controller/Shop/api/ShopController.java
+++ src/main/java/it/unibo/controller/Shop/api/ShopController.java
@@ -1,5 +1,9 @@
 package it.unibo.controller.Shop.api;
 
+import java.util.List;
+
+import it.unibo.model.Shop.api.Skin;
+
 public interface ShopController {
     
     /**
@@ -24,5 +28,29 @@ public interface ShopController {
      * @param skinId The ID of the skin to select
      */
     public void selectSkin(String skinId);
+
+    /**
+     * Gets the list of available skins.
+     * @return A List of available skins
+     */
+    public List<Skin> getAvailableSkins();
+
+    /**
+     * Gets the currently selected skin.
+     * @return The selected skin 
+     */
+    public Skin getSelectedSkin();
+
+    /**
+     * Gets a skin by its ID.
+     * @param skinId The ID of the skin to get
+     * @return The skin with the specified ID, or null if not found
+     */
+    public Skin getSkinById(String id);
+
+    /**
+     * Returns to the main menu.
+     */
+    public void returnToMainMenu();
     
 }
