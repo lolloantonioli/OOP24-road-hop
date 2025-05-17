16ccbd007f264be042b432d240c79737cb7075b1
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-05-17 11:32:10 +0200

    Created MenuView api

---
+++ src/main/java/it/unibo/view/Menu/impl/MenuViewImpl.java
@@ -0,0 +1,41 @@
+package it.unibo.view.Menu.impl;
+
+import java.awt.Font;
+import java.awt.Graphics;
+
+import it.unibo.controller.Menu.api.MenuController;
+import it.unibo.view.Menu.api.MenuView;
+
+public class MenuViewImpl implements MenuView {
+
+    @Override
+    public void render(Graphics g) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+
+    @Override
+    public void updateDimensions(int width, int height) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateDimensions'");
+    }
+
+    @Override
+    public Font scaleFont(Font font) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'scaleFont'");
+    }
+
+    @Override
+    public boolean handleClick(int x, int y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleClick'");
+    }
+
+    @Override
+    public void setController(MenuController controller) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setController'");
+    }
+    
+}
