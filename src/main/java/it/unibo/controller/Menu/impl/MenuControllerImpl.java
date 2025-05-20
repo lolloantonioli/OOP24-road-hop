20203cb76cfad213604b5c8be714fef39f525b88
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-05-20 14:25:27 +0200

    Completed MenuController

--- src/main/java/it/unibo/controller/Menu/impl/MenuControllerImpl.java
+++ src/main/java/it/unibo/controller/Menu/impl/MenuControllerImpl.java
@@ -2,17 +2,21 @@ package it.unibo.controller.Menu.impl;
 
 import it.unibo.controller.GameStateManager;
 import it.unibo.controller.Menu.api.MenuController;
+import it.unibo.view.Menu.api.MenuView;
 
 public class MenuControllerImpl implements MenuController {
-   
+    
+    private final MenuView view;
     private final GameStateManager gameStateManager;
     
     /**
      * Builder for the controller of the menu.
      * @param gameStateManager The game state manager
      */
-    public MenuControllerImpl(GameStateManager gameStateManager) {
+    public MenuControllerImpl(MenuView view,GameStateManager gameStateManager) {
+        this.view = view;
         this.gameStateManager = gameStateManager;
+        this.view.setController(this);
     }
 
     @Override
@@ -32,14 +36,11 @@ public class MenuControllerImpl implements MenuController {
 
     @Override
     public void updateViewDimensions(int width, int height) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateViewDimensions'");
-    }
+        view.updateDimensions(width, height);}
 
     @Override
     public void handleClick(int x, int y) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleClick'");
+       view.handleClick(x, y);
     }
    
 }
