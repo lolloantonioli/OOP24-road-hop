f8e5c3c3b28e16ec5db6678fa716894dae3633fe
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-05-26 14:23:35 +0200

    Modified Frame

--- src/main/java/it/unibo/view/GameFrameImpl.java
+++ src/main/java/it/unibo/view/GameFrameImpl.java
@@ -1,14 +1,19 @@
 package it.unibo.view;
 
 import javax.swing.JFrame;
+
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
 public class GameFrameImpl extends JFrame implements GameFrame {
 
+    private final MenuPanel menuPanel;
+
     public GameFrameImpl() {
         super("Road Hop");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuPanel = new MenuPanelImpl();
     }
 
     @Override
@@ -17,6 +22,7 @@ public class GameFrameImpl extends JFrame implements GameFrame {
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
 
+        this.setContentPane((Container) menuPanel);
         this.setSize(sw / 3, sh / 3);
         this.setLocationByPlatform(true);
         this.setVisible(true);
