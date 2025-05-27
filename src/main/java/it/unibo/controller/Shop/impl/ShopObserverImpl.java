2fad17091433086a2c588b41f2a1b8acfa4013d9
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-05-27 17:13:58 +0200

    added methods to ShopObserver

--- src/main/java/it/unibo/controller/Shop/impl/ShopObserverImpl.java
+++ src/main/java/it/unibo/controller/Shop/impl/ShopObserverImpl.java
@@ -1,7 +1,11 @@
 package it.unibo.controller.Shop.impl;
 
+import java.util.List;
+
 import it.unibo.controller.MainController;
 import it.unibo.controller.Shop.api.ShopObserver;
+import it.unibo.model.Shop.api.ShopModel;
+import it.unibo.model.Shop.api.Skin;
 import it.unibo.view.ShopView;
 
 public class ShopObserverImpl implements ShopObserver{
@@ -9,23 +13,35 @@ public class ShopObserverImpl implements ShopObserver{
 
     private final MainController mainController;
     private final ShopView shopPanel;
+    private final ShopModel shopModel;
 
 
-    public ShopObserverImpl(final MainController mainController, final ShopView shopPanel) {
+    public ShopObserverImpl(final MainController mainController, final ShopView shopPanel, final ShopModel shopModel) {
         this.mainController = mainController;
         this.shopPanel = shopPanel;
+        this.shopModel = shopModel;
     }
+
 
     @Override
     public void activate() {
         shopPanel.setOnBackToMainMenu(() -> mainController.goToMenu());
 
-        shopPanel.setOnSkinPurchase((skinName, price) -> {
-            
+        shopPanel.setOnSkinPurchase((skinId, price) -> {
+            shopModel.selectSkin(skinId);
+            updateView();
         });
 
         
 
+    }
+
+
+    private void updateView() {
+        shopPanel.setCoins(shopModel.getCoins());
+        
+        List<Skin> skins = shopModel.getAllSkins();
+        shopPanel.setSkins(skins);
     }
     
 }
