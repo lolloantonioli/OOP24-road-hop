ca7f60a4bbed836ff9a7945558844575bb1e39a4
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-28 10:27:00 +0200

    Added controls

--- src/main/java/it/unibo/controller/Instructions/impl/InstructionsObserverImpl.java
+++ src/main/java/it/unibo/controller/Instructions/impl/InstructionsObserverImpl.java
@@ -1,5 +1,7 @@
 package it.unibo.controller.Instructions.impl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import it.unibo.controller.MainController;
 import it.unibo.controller.Instructions.api.InstructionsObserver;
 import it.unibo.view.InstructionsPanel;
@@ -9,9 +11,12 @@ public class InstructionsObserverImpl implements InstructionsObserver {
     private final MainController mainController;
     private final InstructionsPanel instructionsPanel;
 
+    private static final String MSG_CONTROLLER = "MainController cannot be null";
+    private static final String MSG_PANEL = "InstructionsPanel cannot be null";
+
     public InstructionsObserverImpl(final MainController mainController, final InstructionsPanel instructionsPanel) {
-        this.mainController = mainController;
-        this.instructionsPanel = instructionsPanel;
+        this.mainController = checkNotNull(mainController, MSG_CONTROLLER);
+        this.instructionsPanel = checkNotNull(instructionsPanel, MSG_PANEL);
     }
 
     public void activate() {
