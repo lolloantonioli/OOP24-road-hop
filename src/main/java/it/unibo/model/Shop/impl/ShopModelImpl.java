08321255faaf9a947d2c3acd15eafe9900b0e80e
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-05-29 11:55:13 +0200

    Created a data manager to save when app's closed

--- src/main/java/it/unibo/model/Shop/impl/ShopModelImpl.java
+++ src/main/java/it/unibo/model/Shop/impl/ShopModelImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import it.unibo.model.Shop.api.ShopModel;
 import it.unibo.model.Shop.api.Skin;
+import it.unibo.model.Shop.impl.ShopDataManagerImpl.ShopSaveData;
+import it.unibo.model.Shop.impl.ShopDataManagerImpl.SkinSaveData;
 
 public class ShopModelImpl implements ShopModel {
 
@@ -14,21 +16,53 @@ public class ShopModelImpl implements ShopModel {
 
     public ShopModelImpl() {
         this.skins = new ArrayList<>();
-        this.coins = 100;
-        initializeSkins();
+        loadShopData();
     }
 
-    private void initializeSkins() {
-        Skin defaultSkin = new SkinImpl("Default", "Frog", 0, true);
-        defaultSkin.select(); 
-        skins.add(defaultSkin);
-        selectedSkin = defaultSkin;
+    private void loadShopData() {
+        ShopSaveData saveData = ShopDataManagerImpl.loadShopData();
+        this.coins = saveData.coins;
+        
+        initializeSkins(saveData);
 
-        skins.add(new SkinImpl("red", "Red Frog", 50, false));
-        skins.add(new SkinImpl("blue", "Blue Frog", 75, false));
-        skins.add(new SkinImpl("gold", "Golden Frog", 150, false));
-        skins.add(new SkinImpl("rainbow", "Rainbow Frog", 200, false));
-    
+        selectedSkin(saveData.selectedSkin);
+    }
+
+    private void selectedSkin(String selectedSkinId) {
+        selectedSkin = skins.stream()
+                .filter(skin -> skin.getId().equals(selectedSkinId))
+                .findFirst()
+                .orElse(skins.get(0));
+    }
+
+    private void initializeSkins(ShopSaveData saveData) {
+       createSkin("Default", "Frog", 0 ,saveData);
+        createSkin("red", "Red Frog", 50, saveData);
+        createSkin("blue", "Blue Frog", 75, saveData);
+        createSkin("gold", "Golden Frog", 150, saveData);
+        createSkin("rainbow", "Rainbow Frog", 200, saveData);
+    }
+
+    private void createSkin(String id, String name, int price, ShopSaveData saveData) {
+        SkinSaveData skinData = saveData.skins.stream()
+                .filter(data -> data.id.equals(id))
+                .findFirst()
+                .orElse(null);
+        
+        boolean unlocked = (skinData != null) ? skinData.unlocked : (id.equals("Default"));
+        boolean selected = (skinData != null) ? skinData.selected : false;
+
+        Skin skin = new SkinImpl(id, name, price, unlocked);
+        if (selected) {
+            skin.select();
+        } 
+        skins.add(skin);
+    }
+
+
+    private void saveData() {
+        String selectedSkinId = (selectedSkin != null) ? selectedSkin.getId() : "Default";
+        ShopDataManagerImpl.saveShopData(skins, selectedSkinId, coins);
     }
 
     @Override
@@ -87,5 +121,15 @@ public class ShopModelImpl implements ShopModel {
     public int getCoins() {
         return coins;
     }
+
+    public void addCoins(int amount) {
+        if (amount > 0) {
+            this.coins += amount;
+            saveData();
+        }
+    }
     
+    public void saveDataToFile(){
+        saveData();
+    }
 }
