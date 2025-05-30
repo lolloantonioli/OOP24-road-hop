f1c0f75e2e17057f7fae4a289ffb48dbe6c263b9
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2025-05-30 17:56:49 +0200

    Modified Collectible types

--- src/main/java/it/unibo/model/Map/util/CollectibleType.java
+++ src/main/java/it/unibo/model/Map/util/CollectibleType.java
@@ -3,7 +3,6 @@ package it.unibo.model.Map.util;
 public final class CollectibleType {
     
     public static final CollectibleType COIN = new CollectibleType("COIN");
-    public static final CollectibleType INVINCIBILITY = new CollectibleType("INVINCIBILITY");
     public static final CollectibleType SECOND_LIFE = new CollectibleType("SECOND_LIFE");
 
     private final String name;
