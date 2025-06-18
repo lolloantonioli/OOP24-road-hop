d3f3ec767cea418ac11254b59c784de2803e622c
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-06-18 12:20:22 +0200

    the end

--- src/test/java/it/unibo/ShopModelExtraTest.java
+++ src/test/java/it/unibo/ShopModelExtraTest.java
@@ -9,13 +9,14 @@ import it.unibo.model.shop.impl.ShopModelImpl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class ShopModelImplExtraTest {
+class ShopModelExtraTest {
+    private static final int COINS_TO_START = 100;
     private ShopModelImpl shopModel;
 
     @BeforeEach
     void setUp() {
         shopModel = new ShopModelImpl();
-        shopModel.addCoins(1000);
+        shopModel.setCoinsForTest(COINS_TO_START);
     }
 
     @Test
--- src/test/java/it/unibo/ShopModelImplTest.java
+++ src/test/java/it/unibo/ShopModelImplTest.java
@@ -1,5 +1,6 @@
 package it.unibo;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,13 +16,20 @@ import java.util.List;
 
 class ShopModelImplTest {
     private static final int COINS_TO_ADD = 50;
+    private static final int COINS_TO_START = 100;
     private static final String COLOR_OF_SKIN = "red";
+    private static final String DEFAUL_STRING = "Default";
     private ShopModelImpl shopModel;
 
     @BeforeEach
     void setUp() {
         shopModel = new ShopModelImpl();
-        shopModel.addCoins(1000); // Assicura abbastanza monete per i test
+        shopModel.setCoinsForTest(COINS_TO_START); // Assicura abbastanza monete per i test
+    }
+
+    @AfterEach
+    void resetShopState() {
+        shopModel.setCoinsForTest(COINS_TO_START);
     }
 
     @Test
@@ -31,6 +39,7 @@ class ShopModelImplTest {
         final Skin selected = shopModel.getSelectedSkin();
         assertEquals(COLOR_OF_SKIN, selected.getId());
         assertTrue(selected.isSelected());
+        shopModel.selectSkin(DEFAUL_STRING);
     }
 
     @Test
