60679943571eae6f6d09fd215c91c017520017f6
Luca Varale Rolla <luca.varalerolla@studio.unibo.it>
2025-06-18 13:43:33 +0200

    now tests don't modify properties

--- src/test/java/it/unibo/PersistenceTest.java
+++ src/test/java/it/unibo/PersistenceTest.java
@@ -8,20 +8,28 @@ import it.unibo.model.shop.impl.ShopModelImpl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterEach;
+
 class PersistenceTest {
     private static final int COINS_TO_ADD = 123;
+    private static final int COINS_TO_START = 100;
     private ShopModelImpl shopModel;
 
     @BeforeEach
     void setUp() {
         shopModel = new ShopModelImpl();
-        shopModel.addCoins(1000);
+        shopModel.setCoinsForTest(COINS_TO_START);
+    }
+
+    @AfterEach
+    void resetMaxScore() {
+        shopModel.resetPersistenceForTest(COINS_TO_START, 0);
     }
 
     @Test
     void testMaxScoreIsUpdatedAndSaved() {
         final int oldMax = shopModel.getMaxScore();
-        final int newScore = oldMax + 10;
+        final int newScore = oldMax + 1;
         shopModel.updateMaxScore(newScore);
         assertEquals(newScore, shopModel.getMaxScore());
     }
@@ -31,7 +39,6 @@ class PersistenceTest {
         final int oldMax = shopModel.getMaxScore();
         shopModel.updateMaxScore(oldMax - 1);
         assertEquals(oldMax, shopModel.getMaxScore());
-        shopModel.updateMaxScore(0);
     }
 
     @Test
@@ -41,4 +48,5 @@ class PersistenceTest {
         final ShopModelImpl reloaded = new ShopModelImpl();
         assertTrue(reloaded.getCoins() >= before + COINS_TO_ADD);
     }
+
 }
